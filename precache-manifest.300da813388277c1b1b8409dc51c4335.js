2ff6d380b1ee7515bca17c9258c781a8c10c149b
Karin Povolozki-Rabichev <karinpovolozki512@gmail.com>
2019-04-23 17:26:41 +0100

    Updates

---
+++ precache-manifest.300da813388277c1b1b8409dc51c4335.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "124f74602dc00707e4149011ada709ec",
+    "url": "/React-Memory_game/static/media/pets_divacolor.124f7460.jpg"
+  },
+  {
+    "revision": "7f0326b858358684d378da2eb2a79597",
+    "url": "/React-Memory_game/static/media/lilsis_pranksta.7f0326b8.jpg"
+  },
+  {
+    "revision": "b935190ddf18691103cd3002fa6cd479",
+    "url": "/React-Memory_game/static/media/bigsis_pranksta.b935190d.jpg"
+  },
+  {
+    "revision": "d439b6c7c57069780b8a44a2a6d3c327",
+    "url": "/React-Memory_game/static/media/background.d439b6c7.jpg"
+  },
+  {
+    "revision": "f2231e17322d99541c00",
+    "url": "/React-Memory_game/static/js/runtime~main.73eccabe.js"
+  },
+  {
+    "revision": "e8058071fe65553801df",
+    "url": "/React-Memory_game/static/js/main.98eaaa32.chunk.js"
+  },
+  {
+    "revision": "71e9c5926c1f5aeee700",
+    "url": "/React-Memory_game/static/js/2.4f0736e0.chunk.js"
+  },
+  {
+    "revision": "e8058071fe65553801df",
+    "url": "/React-Memory_game/static/css/main.1be2fd46.chunk.css"
+  },
+  {
+    "revision": "71e9c5926c1f5aeee700",
+    "url": "/React-Memory_game/static/css/2.42833fdb.chunk.css"
+  },
+  {
+    "revision": "c22742019eb44e030fd9948d99bc94a6",
+    "url": "/React-Memory_game/index.html"
+  }
+];
---
+++ themes/lil_sis/lil_sis-ORIGINAL.js
@@ -0,0 +1,66 @@
+export const lilsisarray = [
+	{
+		id : 1,
+		img : `<img data-id="1" src="themes/lil_sis/lil_80s_bb.jpg" height="100px" width="100px"/>`
+	}, 
+	{
+		id : 2,
+		img : `<img data-id="2" src="themes/lil_sis/lil_babeinthewoods.jpg" height="100px" width="100px"/>`
+	}, 
+	{
+		id : 3,
+		img : `<img data-id="3" src="themes/lil_sis/lil_bigcityBB.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 4,
+		img : `<img data-id="4" src="themes/lil_sis/lil_luxe.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 5,
+		img : `<img data-id="5" src="themes/lil_sis/lil_roller.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 6,
+		img : `<img data-id="5" src="themes/lil_sis/lil_scribbles.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 7,
+		img : `<img data-id="7" src="themes/lil_sis/lil_shimonequeen.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 8,
+		img : `<img data-id="8" src="themes/lil_sis/lil_sk8erboi.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 9,
+		img : `<img data-id="9" src="themes/lil_sis/lil_asifbaby.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 10,
+		img : `<img data-id="10" src="themes/lil_sis/lil_babycartoon.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 11,
+		img : `<img data-id="11" src="themes/lil_sis/lil_dawn.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 12,
+		img : `<img data-id="12" src="themes/lil_sis/lil_kansasQT.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 13,
+		img : `<img data-id="13" src="themes/lil_sis/lil_kittyqueen.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 14,
+		img : `<img data-id="14" src="themes/lil_sis/lil_missjive.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 15,
+		img : `<img data-id="15" src="themes/lil_sis/lil_splatters.jpg" height="100px" width="100px"/>`
+	},
+	{
+		id : 16,
+		img: `<img data-id="16" src="themes/lil_sis/lil_yangQT.jpg" height="100px" width="100px"/>`
+	}
+];
